62f39c4e0f1d8afb0e7b2ae596050c5ec1a7554e
sahusubham2880-maker <sahusubham2880@gmail.com>
2026-09-10 23:14:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ if uploaded_file is not None:
             st.info("No defect detected.")
             st.markdown("---")
             st.write("👨‍💻 Developed by Subham Sahu")
-            st.write("B.Tech 4th Year | Metallurgy Engineering | IGIT, Sarang")
+            st.write("B.Tech 4th Year | Metallurgical and Materials Engineering | IGIT, Sarang")
